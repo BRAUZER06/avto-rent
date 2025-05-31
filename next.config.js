164f1c3302911f2a427c97436630d793d2510690
BRAUZER06 <murtaz.oziev@mail.ru>
2025-05-31 22:00:11 +0300

    fix

--- next.config.js
+++ next.config.js
@@ -1,20 +1,11 @@
 module.exports = {
-
     typescript: {
-    
         ignoreBuildErrors: true,
-      },
+    },
     experimental: {
         missingSuspenseWithCSRBailout: false,
     },
     images: {
-        domains: [
-            "localhost",
-
-            process.env.IMAGES_HOSTNAME,
-            process.env.NEXT_API_URL,
-          
-            
-        ],
+        domains: ["localhost", process.env.IMAGES_HOSTNAME, process.env.NEXT_API_URL],
     },
 };
