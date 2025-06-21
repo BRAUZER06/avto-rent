edc7b4d3bdbd9fec7b359990eaa2b1018c917966
BRAUZER06 <murtaz.oziev@mail.ru>
2025-06-21 13:42:46 +0300

    fix 1

--- src/app/(directories)/banquet-halls/page.tsx
+++
@@ -1,9 +0,0 @@
-import BanquetHallsPageAllPosts from "@src/components/pages/BanquetHallsPages/BanquetHallsPageAllPosts/BanquetHallsPageAllPosts";
-import StandardPage from "@src/components/pages/StandardPage/StandardPage";
-import StandardPageAllPosts from "@src/components/pages/StandardPage/StandardPageAllPosts/StandardPageAllPosts";
-
-export default async function Page() {
-    // return <StandardPageAllPosts />;
-    return <BanquetHallsPageAllPosts />;
-}
- 
