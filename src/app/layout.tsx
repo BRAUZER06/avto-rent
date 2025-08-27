76f2542f4f0ed9c425c37a22030809bdb0a4b727
BRAUZER06 <murtaz.oziev@mail.ru>
2025-08-28 00:25:45 +0300

    fix seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,25 +5,30 @@ import "swiper/css/pagination";
 import MaxWidthWrapper from "@src/components/MaxWidthWrapper/MaxWidthWrapper";
 import { InitAuth } from "@src/lib/hooks/InitAuth";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import YandexMetrika from "./YandexMetrika";
+import GoogleAnalytics from "./GoogleAnalytics";
+import { Metadata } from "next";
 interface RootLayoutProps {
     children: React.ReactNode;
 }
 
+export const metadata: Metadata = {
+    other: {
+        "google-site-verification": "8prA47i9wqJLj9eoI-5BIB4ZCflkbbv27wfA2mlzaqI",
+        "yandex-verification": "d18687635b5cc37c",
+    },
+};
+
 export default function RootLayout({ children }: Readonly<RootLayoutProps>) {
     return (
         <html lang="ru" className="h-full">
-            <Head>
-                <title>Тут название сайта</title>
-                <meta
-                    name="description"
-                    content="Тут должен быть какой-то текст для Seo"
-                />
-            </Head>
             <body
                 className="bg-backgroundBlack relative h-full w-full"
                 suppressHydrationWarning={true}
             >
                 <InitAuth /> {children}
+                <YandexMetrika />
+                <GoogleAnalytics />
             </body>
         </html>
     );
